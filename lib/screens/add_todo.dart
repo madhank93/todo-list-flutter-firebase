f8359f13734514f129d7603cad3371e028aee087
madhank93 <maddymadhan946@gmail.com>
2020-12-29 22:41:00 +0530

    ref: add screen has been refractored

--- lib/screens/add_todo.dart
+++ lib/screens/add_todo.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:todo_app_with_flutter_and_firebase/models/task.dart';
 import 'package:todo_app_with_flutter_and_firebase/models/todo.dart';
 import 'package:todo_app_with_flutter_and_firebase/service/todo_service.dart';
 
-class AddTodo extends StatelessWidget {
+class AddTodo extends StatefulWidget {
+  @override
+  _AddTodoState createState() => _AddTodoState();
+}
+
+class _AddTodoState extends State<AddTodo> {
   final TextEditingController _title = TextEditingController();
-  final TextEditingController _description = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
 
-  final List<Task> _taskList = [Task.fromJson({}), Task.fromJson({})];
+  final List<Task> _taskList = [Task.fromJson({})];
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.lightBlueAccent,
+          child: Icon(Icons.add, color: Colors.white),
+          onPressed: () {
+            if (_taskList.last.taskDescription == null ||
+                _taskList.last.taskDescription.isEmpty) {
+              Fluttertoast.showToast(msg: "Please enter task");
+            } else {
+              setState(
+                () {
+                  _taskList.add(Task.fromJson({}));
+                },
+              );
+            }
+          },
+        ),
         appBar: AppBar(
           title: Text("Add Todo"),
           actions: [
@@ -75,97 +102,102 @@ class AddTodo extends StatelessWidget {
                     height: 15,
                   ),
                   _taskList.length == 1
-                      ? Row(
-                          children: [
-                            Flexible(
-                              child: TextFormField(
-                                controller: _description,
-                                obscureText: false,
-                                textAlign: TextAlign.start,
-                                maxLines: null,
-                                autofocus: false,
-                                cursorColor: Colors.blue,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                    borderSide: BorderSide(
-                                      style: BorderStyle.none,
-                                    ),
-                                  ),
-                                  labelText: "Task 1",
-                                  floatingLabelBehavior:
-                                      FloatingLabelBehavior.always,
-                                  fillColor: Colors.black,
-                                  filled: true,
-                                  contentPadding: EdgeInsets.all(14),
-                                ),
-                                validator: (value) {
-                                  if (value.isEmpty) {
-                                    return 'Please enter task';
-                                  }
-                                  return null;
-                                },
+                      ? TextFormField(
+                          initialValue: _taskList[0].taskDescription,
+                          obscureText: false,
+                          textAlign: TextAlign.start,
+                          maxLines: null,
+                          autofocus: false,
+                          cursorColor: Colors.blue,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                style: BorderStyle.none,
                               ),
                             ),
-                            IconButton(
-                                icon: Icon(
-                                  Icons.add,
-                                  color: Colors.greenAccent,
-                                ),
-                                onPressed: () {})
-                          ],
-                        )
-                      : ListView.builder(
-                          shrinkWrap: true,
-                          primary: false,
-                          itemCount: _taskList.length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: 10.0),
-                              child: Row(
-                                children: [
-                                  Flexible(
-                                    child: TextFormField(
-                                      controller: _description,
-                                      obscureText: false,
-                                      textAlign: TextAlign.start,
-                                      maxLines: null,
-                                      autofocus: false,
-                                      cursorColor: Colors.blue,
-                                      decoration: InputDecoration(
-                                        border: OutlineInputBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          borderSide: BorderSide(
-                                            style: BorderStyle.none,
-                                          ),
-                                        ),
-                                        labelText: "Task ${index + 1}",
-                                        floatingLabelBehavior:
-                                            FloatingLabelBehavior.always,
-                                        fillColor: Colors.black,
-                                        filled: true,
-                                        contentPadding: EdgeInsets.all(14),
-                                      ),
-                                      validator: (value) {
-                                        if (value.isEmpty) {
-                                          return 'Please enter task';
-                                        }
-                                        return null;
-                                      },
-                                    ),
-                                  ),
-                                  IconButton(
-                                      icon: Icon(
-                                        Icons.delete_forever,
-                                        color: Colors.redAccent,
-                                      ),
-                                      onPressed: () {})
-                                ],
-                              ),
-                            );
+                            labelText: "Task 1",
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            fillColor: Colors.black,
+                            filled: true,
+                            contentPadding: EdgeInsets.all(14),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              _taskList[0].taskDescription = value;
+                            });
                           },
-                        ),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter task';
+                            }
+                            return null;
+                          },
+                        )
+                      : _taskList.length > 1
+                          ? ListView.builder(
+                              shrinkWrap: true,
+                              primary: false,
+                              itemCount: _taskList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                Task _task = _taskList[index];
+                                return Padding(
+                                  key: ObjectKey(_task),
+                                  padding: const EdgeInsets.only(bottom: 10.0),
+                                  child: Row(
+                                    children: [
+                                      Flexible(
+                                        child: TextFormField(
+                                          initialValue:
+                                              _taskList[index].taskDescription,
+                                          obscureText: false,
+                                          textAlign: TextAlign.start,
+                                          maxLines: null,
+                                          autofocus: false,
+                                          cursorColor: Colors.blue,
+                                          decoration: InputDecoration(
+                                            border: OutlineInputBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                              borderSide: BorderSide(
+                                                style: BorderStyle.none,
+                                              ),
+                                            ),
+                                            labelText: "Task ${index + 1}",
+                                            floatingLabelBehavior:
+                                                FloatingLabelBehavior.always,
+                                            fillColor: Colors.black,
+                                            filled: true,
+                                            contentPadding: EdgeInsets.all(14),
+                                          ),
+                                          validator: (value) {
+                                            if (value.isEmpty) {
+                                              return 'Please enter task';
+                                            }
+                                            return null;
+                                          },
+                                          onChanged: (value) {
+                                            _taskList[index].taskDescription =
+                                                value;
+                                          },
+                                        ),
+                                      ),
+                                      IconButton(
+                                          icon: Icon(
+                                            Icons.delete_forever,
+                                            color: Colors.redAccent,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              _taskList.removeAt(index);
+                                            });
+                                          })
+                                    ],
+                                  ),
+                                );
+                              },
+                            )
+                          : Container(),
                 ],
               ),
             ),
@@ -179,6 +211,7 @@ class AddTodo extends StatelessWidget {
     if (_formKey.currentState.validate()) {
       Todo todo = new Todo();
       todo.todoTitle = _title.text;
+      todo.taskList = _taskList;
       TodoService().add(todo.toJson());
       Navigator.pop(context);
     }
